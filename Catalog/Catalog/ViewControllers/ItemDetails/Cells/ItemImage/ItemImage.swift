6b0c367f97f06a7ecfa220c25c75b62e0cc86325
Artur Radziukhin <sftawitkakpro@gmail.com>
2021-05-13 13:40:18 +0300

    refactor: sdwedimage, group files

---
+++ Catalog/Catalog/ViewControllers/ItemDetails/Cells/ItemImage/ItemImage.swift
@@ -0,0 +1,23 @@
+//
+//  ItemImage.swift
+//  Catalog
+//
+//  Created by Artur Radziukhin on 1.05.21.
+//
+
+import UIKit
+
+class ItemImage: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Catalog/Catalog/ViewControllers/ItemDetails/Cells/SizeChart/SizeCell/SizeCell.swift
@@ -0,0 +1,23 @@
+//
+//  SizeCell.swift
+//  Catalog
+//
+//  Created by Artur Radziukhin on 1.05.21.
+//
+
+import UIKit
+
+class SizeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
